d03f61a2700b43eb130f36b888196b39645b18f5
Jeremyyyyyyy <80373243+Jeremyyyyyyy@users.noreply.github.com>
2022-04-01 17:02:43 +0800

    2-8 增加自定义配置项示例

--- src/main/java/com/jeremy/wiki/controller/TestController.java
+++ src/main/java/com/jeremy/wiki/controller/TestController.java
@@ -1,18 +1,22 @@
 package com.jeremy.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // 接口
 public class TestController {
+    // @Value("${test.hello：TEST}") TEST 为默认值
+    @Value("${test.hello}") //读取配置项
+    private String testHello;
     /*
     GET POST PUT DELETE
     /user?id=1
      */
     @GetMapping("/hello")
     public String hello(){
-        return "Hello World!";
+        return "Hello World!" + testHello;
     }
 
     @PostMapping("/hello/post")
