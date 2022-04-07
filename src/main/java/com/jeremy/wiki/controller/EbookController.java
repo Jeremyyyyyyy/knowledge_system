83e724ec3a23b79dcffbca6623c72f700b0dbeed
Jeremyyyyyyy <80373243+Jeremyyyyyyy@users.noreply.github.com>
2022-04-07 19:51:49 +0800

    3-6 完成电子书列表查询接口

---
+++ src/main/java/com/jeremy/wiki/controller/EbookController.java
@@ -0,0 +1,23 @@
+package com.jeremy.wiki.controller;
+
+import com.jeremy.wiki.domain.Ebook;
+import com.jeremy.wiki.service.EbookService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController // 接口
+@RequestMapping("/ebook")
+public class EbookController {
+
+    @Resource
+    private EbookService ebookService;
+
+    @GetMapping("/list")
+    public List<Ebook> list(){
+        return ebookService.list();
+    }
+}
---
+++ src/main/java/com/jeremy/wiki/service/EbookService.java
@@ -0,0 +1,20 @@
+package com.jeremy.wiki.service;
+
+import com.jeremy.wiki.domain.Ebook;
+import com.jeremy.wiki.mapper.EbookMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class EbookService {
+
+    @Resource
+    private EbookMapper ebookMapper;
+
+    public List<Ebook> list(){
+        return ebookMapper.selectByExample(null);
+    }
+
+}
